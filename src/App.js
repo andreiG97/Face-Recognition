efd9c794ac2488f34435de4252f2fe613184752e
Andrei G <andrei.grideanu@gmail.com>
2022-06-20 16:51:14 +0300

    api fixes

--- src/App.js
+++ src/App.js
@@ -7,62 +7,94 @@ import React, { useState } from 'react';
 import FaceRecognition from './components/FaceRecognition';
 
 
+
+
 function App() {
 
   let [input, setInput] = useState('');
   let [imageUrl, setImageUrl] = useState('');
+  let [box, setBox] = useState({});
 
+  const USER_ID = 'andreippa97';
+  const PAT = 'f57ba082a2454498b51e3e82215effbb';
+  const APP_ID = 'face-recognition';
+  const MODEL_ID = 'face-detection';
+  const MODEL_VERSION_ID = '45fb9a671625463fa646c3523a3087d5';
+  const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
+
+  
+
+  function calculateFaceLocation(data){
+     const clarifaiFace = JSON.parse(data, null, 2).outputs[0].data.regions[0]
+      .region_info.bounding_box;
+     const image = document.getElementById("inputImage");
+     const width = Number(image.width);
+     const height = Number(image.height);
+     return {
+       leftCol: clarifaiFace.left_col * width,
+       topRow: clarifaiFace.top_row * height,
+       rightCol: width - clarifaiFace.right_col * width,
+       bottomRow: height - clarifaiFace.bottom_row * height,
+     };
+
+  }
+
+  function displayFaceBox(wrap){
+    console.log(wrap);
+    setBox({box: wrap});
+  }
 
   function onChangeHandler(e){
     setInput(e.target.value);
     
   }
 
+
   function onSubmit(){
     setImageUrl(input);
-    //console.log(imageUrl);
+   console.log(box); 
    
-    const raw = JSON.stringify({
-      user_app_id : {
-        user_id: "b8j91wnrp03s",
-        app_id: "72a7142cee434e9a9a6f98db5ff1bd82"
-      },
-      inputs: [
+   const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
         {
-          data: {
-            image: {
-              url: input
-            },
-          },
-        },
-      ],
-    });
- 
-    fetch(
-       "https://api.clarifai.com/v2/models/f76196b43bbd45c99b4f3cd8e8b40a8a/outputs",
-       {
-         method: "POST",
-         headers: {
-           Accept: "application/json",
-           Authorization: "1e45ba3664b64452a5020a44253e3175",
-         },
-         body: raw,
-       }
-     )
-     .then((response) => response.text())
-     .then((result) => this.displayFaceBox(this.calculateFaceLocation(result)))
-     .catch((error) => console.log("error", error));
-  
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+});
 
+const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+};
+
+
+fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+    .then((response) => response.text())
+    .then((result) => displayFaceBox(calculateFaceLocation(result)))
+    .catch(error => console.log('error', error));
+    
   }
 
   return (
     <div className="App">
+      
         <Navigation/>
         <Logo/>
         <Rank/>
         <LinkForm onChangeHandler={onChangeHandler} onSubmit={ onSubmit }/>
-        <FaceRecognition imageUrl={imageUrl}/>
+        <FaceRecognition box={box} imageUrl={imageUrl}/>
         
     </div>
   );
--- src/components/FaceRecognition.jsx
+++ src/components/FaceRecognition.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import './FaceRecognition.css';
 
 function FaceRecognition(props) {
+  let {imageUrl, box} = props;
+//nested box object
+  console.log(box)
   return (
     <div className='center ma'>
         <div className='absolute mt2'>
-            <img src={props.imageUrl} alt="" width="400px" height='auto' />
+            <img id="inputImage" src={imageUrl} alt="" width="500px" height='auto' />
+            <div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
         </div>
     </div>
   )
